5a542f71e12ac2f4b25ad16c378bc9146afbd9f3
Isis Ariele Araújo Duarte <isisariele1@gmail.com>
2025-05-02 17:37:00 -0300

    test: add user login test

---
+++ tests/authentication/test_auth_login.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from users.models import CustomUser
+from rest_framework.test import APIClient
+
+class TestAuthLogin(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        CustomUser.objects.create(
+            username="testuser",
+            password="testuserpassword",
+            name="Test User",
+            email="test@user.com"
+        )
+
+    def test_should_login_user_successfully(self):
+        response = self.client.post(
+            "/auth/login/",
+            {
+                "username": "testuser",
+                "password": "testuserpassword"
+            }
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("access", response.data)
+        self.assertIn("refresh", response.data)
+
+
+    def test_should_return_error_when_username_or_password_is_incorrect(self):
+        response = self.client.post(
+            "/auth/login/",
+            {
+                "username": "wronguser",
+                "password": "wrongpassword"
+            }
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data["detail"], "No active account found with the given credentials")

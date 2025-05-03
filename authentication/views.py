3eaccea53e90ba0313e7232b438ceb4a3f951097
Isis Ariele Araújo Duarte <isisariele1@gmail.com>
2025-05-03 16:10:42 -0300

    feat: add profile_picture and bio to CustomUserSerializer

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from authentication.serializers import CustomUserSerializer
+from users.serializers import CustomUserSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
--- users/serializers.py
+++ users/serializers.py
@@ -5,14 +5,16 @@ from users.models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'name', 'password']
+        fields = ['username', 'email', 'name', 'password', 'profile_picture', 'bio']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = CustomUser(
             name=validated_data['name'],
             email=validated_data['email'],
-            username=validated_data['username']
+            username=validated_data['username'],
+            profile_picture=validated_data.get('profile_picture'),
+            bio=validated_data.get('bio')
         )
         user.set_password(validated_data['password'])
         user.save()

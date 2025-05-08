e0afeeb033412dbc8f218c02ce1404e052b7a2c6
Isis Ariele Araújo Duarte <isisariele1@gmail.com>
2025-05-08 20:41:10 -0300

    feat: add black to CI

--- tests/authentication/test_auth_login.py
+++ tests/authentication/test_auth_login.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from users.models import UserModel
 from rest_framework.test import APIClient
 
+
 class TestAuthLogin(TestCase):
     def setUp(self):
         self.client = APIClient()
@@ -9,31 +10,25 @@ class TestAuthLogin(TestCase):
             username="testuser",
             password="testuserpassword",
             name="Test User",
-            email="test@user.com"
+            email="test@user.com",
         )
 
     def test_should_login_user_successfully(self):
         response = self.client.post(
-            "/auth/login/",
-            {
-                "username": "testuser",
-                "password": "testuserpassword"
-            }
+            "/auth/login/", {"username": "testuser", "password": "testuserpassword"}
         )
 
         self.assertEqual(response.status_code, 200)
         self.assertIn("access", response.data)
         self.assertIn("refresh", response.data)
 
-
     def test_should_return_error_when_username_or_password_is_incorrect(self):
         response = self.client.post(
-            "/auth/login/",
-            {
-                "username": "wronguser",
-                "password": "wrongpassword"
-            }
+            "/auth/login/", {"username": "wronguser", "password": "wrongpassword"}
         )
 
         self.assertEqual(response.status_code, 401)
-        self.assertEqual(response.data["detail"], "No active account found with the given credentials")
+        self.assertEqual(
+            response.data["detail"],
+            "No active account found with the given credentials",
+        )
